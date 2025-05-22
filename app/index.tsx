4d367850ede83f7ffa838826de835f26cf8ae504
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-05-22 17:42:01 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  const name = "ryan";
+
+  return (
+    <View>
+      <Text>Olá {name}</Text>
+    </View>
+  );
+}
