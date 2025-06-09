0cb0a278c18c504a10a60ab4337804cf3760cc9b
ryanmarinhodev <ryanmarinhodev@gmail.com>
2025-06-09 10:55:08 -0300

    Adicionando estilo criado ao modal

--- src/components/modalComponent/modal.tsx
+++ src/components/modalComponent/modal.tsx
@@ -8,15 +8,27 @@ interface ModalProps {
 }
 
 function ModalComponent({ visible, onClose }: ModalProps) {
+  const handleSave = () => {
+    alert("Salvo com sucesso!");
+    onClose();
+  };
+
   return (
     <Modal visible={visible} onRequestClose={() => onClose} transparent={true}>
       <View style={styles.viewModal}>
         <View style={styles.modalStyleBox}>
           <Text style={styles.modalText}>Modal Aberto</Text>
           <Text style={styles.modalParagraph}>Parágrafo do modal</Text>
-          <TouchableOpacity style={styles.closeModalButton} onPress={onClose}>
-            <Text style={styles.modalButtonText}>Fechar Modal</Text>
-          </TouchableOpacity>
+          <View style={styles.modalButtonsPai}>
+            <TouchableOpacity style={styles.closeModalButton} onPress={onClose}>
+              <Text style={styles.modalButtonText} onPress={handleSave}>
+                Salvar
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.closeModalButton} onPress={onClose}>
+              <Text style={styles.modalButtonText}>Fechar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </Modal>
